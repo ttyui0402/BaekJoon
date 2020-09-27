7370f8f269b0b43b81f98b05ef178b62b3170f47
Yui <64628448+ttyui0402@users.noreply.github.com>
2020-09-27 15:55:43 +0900

    .

---
+++ StarEx4.java
@@ -0,0 +1,27 @@
+package BaekJoon;
+
+public class StarEx4 {
+
+	public static void main(String[] args) {
+		
+//		Scanner scanner = new Scanner(System.in);
+//		int userInput = Integer.parseInt(scanner.nextLine());
+//		
+//		for (int i = userInput; i >= 1; i--) {
+//			for (int j = userInput; j >= 1; j--) {
+//				if(j>i) {
+//					System.out.print("*");
+//				} else {
+//					System.out.print(" ");
+//				}
+//			}
+//			System.out.println();
+		}
+	}
+
+	
+					
+				
+	
+		
+		
---
+++ StatEx5.java
@@ -0,0 +1,16 @@
+package BaekJoon;
+
+public class StatEx5 {
+
+	public static void main(String[] args) {
+		// 1번
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= 5; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
