f8a31fb5bfb24b9ee3276e484b56ff8622a423d0
ttyui0402 <ttyui0402@gmail.com>
2021-01-10 14:27:54 +0900

    별찍기

---
+++ StarEx14.java
@@ -0,0 +1,24 @@
+
+public class StarEx14 {
+
+	public static void main(String[] args) {
+		// 9번
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= 5; j++) {
+//				if (i+j==2 || i+j==3 || i+j==4 || i+j==5 || i+j==6) {
+				if (!((i+j>6 || i>j) && (i+j<6 || i<j))) {	
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
+					
+		
+
+// (i+j>=6 || i>=j)
+//i>=j
+//!(i+j<=6 || i<=j)
